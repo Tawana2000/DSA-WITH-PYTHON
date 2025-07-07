3da83f68828483cd1664f5444f0b81b5a58e2933
Tawana2000 <murtezatawana8@gmail.com>
2025-07-06 23:41:23 -0700

    Add files via upload

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example2.py
@@ -0,0 +1,15 @@
+# Write a program to sort the list items in descending order using bubble sort
+
+def bubble_sort(lst):
+
+    for i in range(len(lst)):
+        for j in range(len(lst) - 1):
+
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+
+    return lst
+
+result = list(map(int, input().split()))  # Take integer input and convert it to a list
+print(bubble_sort(result))
