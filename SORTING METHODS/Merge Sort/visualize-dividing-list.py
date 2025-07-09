f386db5e05414f9e4175e074617457d897466ebd
Tawana2000 <murtezatawana8@gmail.com>
2025-07-09 13:38:05 -0700

    Create visualize-dividing-list.py

---
+++ SORTING METHODS/Merge Sort/visualize-dividing-list.py
@@ -0,0 +1,23 @@
+# Visualize dividing the list recursively 
+
+def merge_sort(lst):
+
+    if len(lst) <= 1:
+        return lst
+    
+    print(f'lst: {lst}')
+
+    mid = len(lst) // 2
+
+    print(f'mid: {lst[mid]}')
+
+    left = merge_sort(lst[:mid])
+    print(f'Left Partition: {left}')
+
+    right = merge_sort(lst[mid:])
+    print(f'Right Partition: {right}')
+
+    return "---"
+
+lst = [5, 4, 7, 3, 2, 12]
+merge_sort(lst)
