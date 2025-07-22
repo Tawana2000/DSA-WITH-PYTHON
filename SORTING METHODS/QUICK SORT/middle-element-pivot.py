1287ca88ecb8f63258e14f73423a0e9ec7e8a4f1
Tawana2000 <murtezatawana8@gmail.com>
2025-07-22 13:41:44 -0700

    Update middle-element-pivot.py

--- SORTING METHODS/QUICK SORT/middle-element-pivot.py
+++ SORTING METHODS/QUICK SORT/middle-element-pivot.py
@@ -1,1 +1,24 @@
+# Write a quick sort program that sorts elements in ascending order. However, choose the middle element as the pivot
 
+def quick_sort(lst):
+
+    if len(lst) <= 1:
+        return lst
+    
+    else:
+        pivot = lst.pop(len(lst) // 2)
+
+    left = []
+    right = []
+
+    for elements in lst:
+        if elements > pivot:
+            right.append(elements)
+
+        else:
+            left.append(elements)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+lst = list(map(int, input("Enter the list elements: ").split()))
+print(quick_sort(lst))
