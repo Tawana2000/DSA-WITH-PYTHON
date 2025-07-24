d44fc16a6812e684ab63807cb21f47a5b889d4ec
Tawana2000 <murtezatawana8@gmail.com>
2025-07-24 12:58:42 -0700

    Add files via upload

---
+++ SORTING METHODS/QUICK SORT/quick-sort-ascending-order.py
@@ -0,0 +1,28 @@
+# Write a quick sort program to sort the elements of the list in ascending order
+
+def quick_sort(lst):
+
+    lst_lenght = len(lst)
+
+    if lst_lenght <= 1:
+        return lst
+    
+    else:
+        pivot = lst.pop()
+
+    left_elements = []
+    right_elements = []
+
+    for elements in lst:
+        if elements < pivot:
+            left_elements.append(elements)
+
+        else:
+            right_elements.append(elements)
+
+    return quick_sort(left_elements) + [pivot] + quick_sort(right_elements)
+
+
+data_list = list(map(int, input("Enter the list elements separated by a space:").split()))
+
+print(quick_sort(data_list))
