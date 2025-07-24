0a2b9ea5099fe51473dd914c9e30088678ccaac6
Tawana2000 <murtezatawana8@gmail.com>
2025-07-24 12:58:58 -0700

    Add files via upload

---
+++ SORTING METHODS/QUICK SORT/quick-sort-descending-order.py
@@ -0,0 +1,24 @@
+# Write a program to sort the list itmes in descending order using quick sort
+
+def quick_sort(lst):
+
+    if len(lst) <= 1:
+        return lst
+    
+    else:
+        pivot = lst.pop()
+
+    left = []
+    right = []
+
+    for elements in lst:
+        if elements > pivot:
+            right.append(elements)
+
+        else:
+            left.append(elements)
+
+    return quick_sort(right) + [pivot] + quick_sort(left)
+
+data_list = list(map(int, input("Enter the list elements: ").split()))
+print(quick_sort(data_list))
