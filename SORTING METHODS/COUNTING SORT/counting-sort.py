45a82323b79bb7fd53d43b64ab7909601578b706
Tawana2000 <murtezatawana8@gmail.com>
2025-08-05 13:53:54 -0700

    Update counting-sort.py

--- SORTING METHODS/COUNTING SORT/counting-sort.py
+++ SORTING METHODS/COUNTING SORT/counting-sort.py
@@ -1,1 +1,16 @@
+def counting_sort(lst):
 
+    if len(lst) <= 1:
+        return lst
+    
+    max_element = max(lst)     #Find the largest element
+    counting_list = [0] * (max_element + 1)      #Create the counting list and set all the values to 0
+
+    for num in lst:
+        counting_list[num] += 1   #Fill the counting list with frequency of each number
+
+    sorted_output = []
+    for index, value in enumerate(counting_list):
+        sorted_output.extend([index] * value)
+
+    return sorted_output
