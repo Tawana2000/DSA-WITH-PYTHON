1069e9ebdca2522ec3974908884e9b95211819fa
Tawana2000 <murtezatawana8@gmail.com>
2025-08-06 14:22:33 -0700

    Update count-unique-elements.py

--- SORTING METHODS/COUNTING SORT/count-unique-elements.py
+++ SORTING METHODS/COUNTING SORT/count-unique-elements.py
@@ -1,1 +1,18 @@
 #Create a program to find the number of unique elements in a list
+
+ if len(lst) <= 1:
+        return lst
+    
+    max_element = max(lst)
+    counting_list = [0] * (max_element + 1)
+
+    for num in lst:
+        counting_list[num] += 1
+
+    unique_count = 0
+
+    for value in counting_list:
+        if value >= 1:
+            unique_count += 1
+
+    return unique_count
