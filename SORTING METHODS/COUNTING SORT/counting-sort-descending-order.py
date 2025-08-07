3167b211bbb0331e0ff7ad643e80b96cceff0da9
Tawana2000 <murtezatawana8@gmail.com>
2025-08-07 12:47:59 -0700

    Update counting-sort-descending-order.py

--- SORTING METHODS/COUNTING SORT/counting-sort-descending-order.py
+++ SORTING METHODS/COUNTING SORT/counting-sort-descending-order.py
@@ -7,3 +7,13 @@ def counting_sort(lst):
         
     max_element = max(lst)
     counting_list = [0] * (max_element + 1)
+    
+    for num in lst:
+        counting_list[num] += 1
+
+    sorted_output = []
+
+    for index in range(len(counting_list) -1, -1, -1):
+        sorted_output.extend([index] * counting_list[index])
+
+    return sorted_output
