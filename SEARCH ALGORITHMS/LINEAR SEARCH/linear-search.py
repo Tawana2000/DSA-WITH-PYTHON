824ccfca660a6e888ae62ba070335b92b0f2fe69
Tawana2000 <murtezatawana8@gmail.com>
2025-08-11 23:34:36 -0700

    Update linear-search.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search.py
@@ -1,1 +1,19 @@
 # Write a function to perform linear search
+
+def linear_search(lst, target):
+
+    for index, element in enumerate(lst):
+        if element == target:
+            return index
+        
+    return None
+
+lst = [1, 4, 5, 12, 3, 50, 21, 16, 23, 7]
+target = int(input())
+
+result = linear_search(lst, target)
+
+if result:
+    print(f"Index of the target value: {result}")
+else:
+    print("Target value not found in the list.")
