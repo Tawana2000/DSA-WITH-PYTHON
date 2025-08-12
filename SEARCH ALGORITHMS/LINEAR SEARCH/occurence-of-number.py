9aee8fa6a9070529c6afcd8437da9bca2fadc367
Tawana2000 <murtezatawana8@gmail.com>
2025-08-12 13:24:09 -0700

    Update occurence-of-number.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/occurence-of-number.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/occurence-of-number.py
@@ -9,3 +9,8 @@ def count_occurrence(lst, n):
             count +=1
 
     return count
+
+lst = [2, 8, 13, 6, 6, 9, 7, 6, 6]
+n = int(input("Enter the target number to find the number of occurrence: "))
+
+print(count_occurrence(lst, n))
