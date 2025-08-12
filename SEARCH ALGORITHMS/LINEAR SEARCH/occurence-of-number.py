63c5fe82a056e3468f5d7573180fe22be71a6fdc
Tawana2000 <murtezatawana8@gmail.com>
2025-08-12 13:23:54 -0700

    Update occurence-of-number.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/occurence-of-number.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/occurence-of-number.py
@@ -1,1 +1,11 @@
+# Create a program to find the occurrence of a given number in a list
 
+def count_occurrence(lst, n):
+
+    count = 0
+
+    for element in lst:
+        if element == n:
+            count +=1
+
+    return count
