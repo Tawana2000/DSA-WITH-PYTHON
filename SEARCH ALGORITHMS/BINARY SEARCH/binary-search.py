2dd9d9d79fca12fdcafa42b2bf20506593a0ca0d
Tawana2000 <murtezatawana8@gmail.com>
2025-08-13 15:10:45 -0700

    Update binary-search.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search.py
@@ -4,3 +4,17 @@ def binary_search(lst, target):
 
     low = 0
     high = len(lst) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+
+        if target == lst[mid]:
+            return mid
+        
+        elif target <= lst[mid]:
+            high = mid - 1
+
+        else:
+            low = mid + 1
+
+    return None
