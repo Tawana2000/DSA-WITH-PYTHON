f1bba8dc6e6d35650aa9169a132a56a92695fd38
Tawana2000 <murtezatawana8@gmail.com>
2025-08-14 12:53:57 -0700

    Update binary-search-using-recursion.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
@@ -20,3 +20,8 @@ def binary_search(lst, target, low, high):
 lst = sorted([12, 4, 8, 3, 2, 9, 11, 23, 5])
 target = int(input("Enter the target number: "))
 result = binary_search(lst, target, 0, len(lst) - 1)
+
+if result:
+    print(f"Element {target} is found at index {result}")
+else:
+    print(f"{target} is not found in the list.")
