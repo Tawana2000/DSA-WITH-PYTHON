f45a9afd17a9f46b7f167e6557d5403f110516c5
Tawana2000 <murtezatawana8@gmail.com>
2025-08-14 12:53:14 -0700

    Update binary-search-using-recursion.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
@@ -1,2 +1,18 @@
 # Write a function to perform Binary Search using recursion
 
+def binary_search(lst, target, low, high):
+
+    if high >= low:
+        mid = (high + low) // 2
+
+        if lst[mid] == target:
+            return mid
+        
+        elif lst[mid] > target:
+            return binary_search(lst, target, low, mid - 1)
+        
+        else:
+            return binary_search(lst, target, mid + 1, high)
+        
+    else:
+        return None
