2e25a9e1b94e27429368846d6f3366b6507167c4
Tawana2000 <murtezatawana8@gmail.com>
2025-08-15 13:57:18 -0700

    Update list_methods.py

--- PYTHON LIST/list_methods.py
+++ PYTHON LIST/list_methods.py
@@ -1,1 +1,12 @@
+# Create a program to perform operations on a list using various list methods.
 
+"""
+Perform these operations on the animals list:
+
+- Add 'Raccoon' to the end of the animals list.
+- Add all the elements from wild_animals to the end of the animals list.
+- Remove the third element from the animals list using the pop() method.
+- Remove the last element from the animals list using the pop() method.
+- Insert 'Moose' at the third position in the animals list.
+- Print the animals list.
+"""
