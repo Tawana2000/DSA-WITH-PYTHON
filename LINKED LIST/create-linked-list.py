83213dc8cf20e1dbfaae2749691448721abdd9f7
Tawana2000 <murtezatawana8@gmail.com>
2025-08-16 23:22:16 -0700

    Update create-linked-list.py

--- LINKED LIST/create-linked-list.py
+++ LINKED LIST/create-linked-list.py
@@ -5,3 +5,20 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    
+    def create_linked_list(self):
+
+        node1 = Node(56)
+        self.head = node1
+
+        node2 = Node(8)
+        node1.next = node2
+
+        node3 = Node(17)
+        node2.next = node3
