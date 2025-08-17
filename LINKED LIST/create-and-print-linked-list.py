16f216dae9491c3cc8bd0329cbdc28f91e45b007
Tawana2000 <murtezatawana8@gmail.com>
2025-08-17 15:27:58 -0700

    Update create-and-print-linked-list.py

--- LINKED LIST/create-and-print-linked-list.py
+++ LINKED LIST/create-and-print-linked-list.py
@@ -6,3 +6,25 @@ class Node:
         self.data = data 
         self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def create_linked_list(self):
+
+        data1 = int(input())
+        data2 = int(input())
+        data3 = int(input())
+        data4 = int(input())
+
+        node1 = Node(data1)
+        self.head = node1
+
+        node2 = Node(data2)
+        node1.next = node2
+
+        node3 = Node(data3)
+        node2.next = node3
+
+        node4 = Node(data4)
+        node3.next = node
