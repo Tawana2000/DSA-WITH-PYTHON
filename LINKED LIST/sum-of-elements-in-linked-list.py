ec8de5a4d0d4b54a1e9cf24f4033da626d7e868f
Tawana2000 <murtezatawana8@gmail.com>
2025-08-19 13:03:56 -0700

    Update sum-of-elements-in-linked-list.py

--- LINKED LIST/sum-of-elements-in-linked-list.py
+++ LINKED LIST/sum-of-elements-in-linked-list.py
@@ -5,3 +5,18 @@ class Node:
 
         self.data = data
         self.next = None
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def creat_linked_list(self):
+
+        node1 = Node(14)
+        node2 = Node(25)
+        node3 = Node(30)
+        node4 = Node(6)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
