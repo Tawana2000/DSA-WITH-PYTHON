2b80dd0197a9ca101322a4c8d7c74596d8eeeef8
Tawana2000 <murtezatawana8@gmail.com>
2025-08-18 20:56:17 -0700

    Update count-number-of-nodes.py

--- LINKED LIST/count-number-of-nodes.py
+++ LINKED LIST/count-number-of-nodes.py
@@ -4,3 +4,19 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None 
+
+    
+    def create_linked_list(self):
+
+        node1 = Node(4)
+        self.head = node1
+
+        node2 = Node(12)
+        node1.next = node2
+
+        node3 = Node(2)
+        node2.next = node3
