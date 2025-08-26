52bcef96833d8aeb2061f1f9c0bcc9242f98bfb9
Tawana2000 <murtezatawana8@gmail.com>
2025-08-26 13:32:35 -0700

    Update delete-a-node-in-given-position.py

--- LINKED LIST/delete-a-node-in-given-position.py
+++ LINKED LIST/delete-a-node-in-given-position.py
@@ -1,2 +1,32 @@
 # Write a program to delete a node in a given position in an existing linked list
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def create_linked_list(self):
+
+        node1 = Node(14)
+        node2 = Node(60)
+        node3 = Node(23)
+        node4 = Node(30)
+        node5 = Node(8)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+
+    def display(self):
+        current = self.head
+        while current:
+            print(f"{current.data}", end="->")
+            current = current.next
+
+        print(None)
