24d3010ccc9cab37436da2e7da078e2163928bf8
Tawana2000 <murtezatawana8@gmail.com>
2025-08-27 15:07:41 -0700

    Update insert-node-at-the-beginning.py

--- LINKED LIST/insert-node-at-the-beginning.py
+++ LINKED LIST/insert-node-at-the-beginning.py
@@ -4,3 +4,30 @@ class Node:
     def __init__(self, data):
          self.data = data
          self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = Node
+
+    def create_linked_list(self):
+
+        node1 = Node(14)
+        node2 = Node(43)
+        node3 = Node(8)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+
+    def insert_node_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(f"{current.data}", end="->")
+            current = current.next
+
+        print(None)
