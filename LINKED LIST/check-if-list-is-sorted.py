3c9dd9ebb0ada29955feaca8765d7333264dbc2e
Tawana2000 <murtezatawana8@gmail.com>
2025-08-28 13:57:29 -0700

    Update check-if-list-is-sorted.py

--- LINKED LIST/check-if-list-is-sorted.py
+++ LINKED LIST/check-if-list-is-sorted.py
@@ -4,3 +4,21 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+
+    def create_linked_list(self):
+
+        node1 = Node(int(input()))
+        node2 = Node(int(input()))
+        node3 = Node(int(input()))
+        node4 = Node(int(input()))
+
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
