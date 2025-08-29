0f3869681b45174cf4e303d8e87b4415aafb0ca5
Tawana2000 <murtezatawana8@gmail.com>
2025-08-29 13:22:45 -0700

    Update largest-number-in-linked-list.py

--- LINKED LIST/largest-number-in-linked-list.py
+++ LINKED LIST/largest-number-in-linked-list.py
@@ -8,3 +8,16 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head = None
+        def create_linked_list(self):
+
+        node1 = Node(5)
+        node2 = Node(3)
+        node3 = Node(18)
+        node4 = Node(23)
+        node5 = Node(7)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
