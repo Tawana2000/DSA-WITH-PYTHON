67cef0a909790faa370da1aabd22b5180cd0e671
Tawana2000 <murtezatawana8@gmail.com>
2025-08-29 13:22:22 -0700

    Update largest-number-in-linked-list.py

--- LINKED LIST/largest-number-in-linked-list.py
+++ LINKED LIST/largest-number-in-linked-list.py
@@ -1,2 +1,10 @@
 # Write a program to find the largest number in an existing linked list
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
