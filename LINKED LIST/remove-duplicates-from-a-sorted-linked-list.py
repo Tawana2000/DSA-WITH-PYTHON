8973ddbe095c3d0b9a6c1aeafa31166304e87df6
Tawana2000 <murtezatawana8@gmail.com>
2025-08-30 15:00:13 -0700

    Update remove-duplicates-from-a-sorted-linked-list.py

--- LINKED LIST/remove-duplicates-from-a-sorted-linked-list.py
+++ LINKED LIST/remove-duplicates-from-a-sorted-linked-list.py
@@ -1,1 +1,11 @@
 # Write a program to remove duplicates from a sorted linked list.
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
