e18069f019193ff68f72430f59ed29a37cd80acf
Tawana2000 <murtezatawana8@gmail.com>
2025-08-30 15:00:39 -0700

    Update remove-duplicates-from-a-sorted-linked-list.py

--- LINKED LIST/remove-duplicates-from-a-sorted-linked-list.py
+++ LINKED LIST/remove-duplicates-from-a-sorted-linked-list.py
@@ -9,3 +9,21 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head = None
+        
+    def create_linked_list(self):
+
+        node1 = Node(8)
+        node2 = Node(12)
+        node3 = Node(12)
+        node4 = Node(12)
+        node5 = Node(15)
+        node6 = Node(18)
+        node7 = Node(18)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = node5
+        node5.next = node6
+        node6.next = node7
