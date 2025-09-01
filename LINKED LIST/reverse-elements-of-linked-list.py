2392f06b00fd4940fecb3c4c1020bac09d226ecb
Tawana2000 <murtezatawana8@gmail.com>
2025-09-01 11:49:40 -0700

    Update reverse-elements-of-linked-list.py

--- LINKED LIST/reverse-elements-of-linked-list.py
+++ LINKED LIST/reverse-elements-of-linked-list.py
@@ -22,3 +22,10 @@ class Linkedlist:
         node1.next = node2
         node2.next = node3
         node3.next = node4
+        def display(self):
+
+        current = self.head
+        while current:
+            print(f"{current.data}", end="->")
+            current = current.next
+        print(None)
