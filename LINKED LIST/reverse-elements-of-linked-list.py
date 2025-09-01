743f57dc9950aa5a19ad4d68358ce9583c8ba59f
Tawana2000 <murtezatawana8@gmail.com>
2025-09-01 11:49:22 -0700

    Update reverse-elements-of-linked-list.py

--- LINKED LIST/reverse-elements-of-linked-list.py
+++ LINKED LIST/reverse-elements-of-linked-list.py
@@ -1,1 +1,24 @@
 # Write a program to reverse the elements of linked list
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    
+    def __init__(self):
+        self.head = None
+
+    def create_linked_list(self):
+
+        node1 = Node(2)
+        node2 = Node(3)
+        node3 = Node(4)
+        node4 = Node(5)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
