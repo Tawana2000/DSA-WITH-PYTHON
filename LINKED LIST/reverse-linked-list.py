e48bedf4c7637464cec4132f632f924becdaac73
Tawana2000 <murtezatawana8@gmail.com>
2025-09-09 12:43:30 -0700

    Update reverse-linked-list.py

--- LINKED LIST/reverse-linked-list.py
+++ LINKED LIST/reverse-linked-list.py
@@ -64,4 +64,23 @@ class LinkedList:
             print(f"{current.data}", end="->")
             current = current.next
         print(None)
+
+
+    #Reverse Linked List
+    def reverse_linked_list(self):
+        prev_node = None
+        current = self.head
+        next_node = current.next
+
+        while True:
+            current.next = prev_node
+            prev_node = current
+            current = next_node
+            next_node = next_node.next
+
+            if next_node == None:
+                current.next = prev_node
+                break
+        
+        self.head = current
        
