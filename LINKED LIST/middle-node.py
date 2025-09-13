104b21ae509cd2ed521e7089c7d6c1a3698661e6
Tawana2000 <murtezatawana8@gmail.com>
2025-09-13 13:49:35 -0700

    Update middle-node.py

--- LINKED LIST/middle-node.py
+++ LINKED LIST/middle-node.py
@@ -1,1 +1,20 @@
 # Write a program to find the middle node
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+
+    def create_linked_list(self):
+        node1 = Node(45)
+        self.head = node1
+
+        node2 = Node(23)
+        node1.next = node2
+
+        node3 = Node(50)
+        node2.next = node3
