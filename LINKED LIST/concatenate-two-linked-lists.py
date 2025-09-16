8d2f479ed25bbf0cc0ec64b33870515efe7e3299
Tawana2000 <murtezatawana8@gmail.com>
2025-09-16 12:49:32 -0700

    Update concatenate-two-linked-lists.py

--- LINKED LIST/concatenate-two-linked-lists.py
+++ LINKED LIST/concatenate-two-linked-lists.py
@@ -4,3 +4,14 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end="->")
+            current = current.next
+    print("None")
