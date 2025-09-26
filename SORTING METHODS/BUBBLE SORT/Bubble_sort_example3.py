7bb7004f0edeb743015fe9408a813d74d131a491
Tawana2000 <murtezatawana8@gmail.com>
2025-09-26 12:46:06 -0700

    Update Bubble_sort_example3.py

--- SORTING METHODS/BUBBLE SORT/Bubble_sort_example3.py
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example3.py
@@ -1,1 +1,18 @@
 #Imagine you are keeping track of the scores of 7 players in a video game [42, 17, 8, 99, 23, 56, 74], use bubble sort to arrange the scores in ascending order
+
+class Bubble_Sort:
+
+    def bubble_sort(self, lst):
+
+        self.lst = lst
+        size = len(lst)
+
+        for i in range(size): 
+            for j in range(size - 1 - i):
+                if lst[j] > lst[j + 1]:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    
+        return lst
+    
+BBS = Bubble_Sort()
+print(BBS.bubble_sort([42, 17, 8, 99, 23, 56, 74]))
