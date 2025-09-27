4816cc582b1d1df21eff31e04e33041536b21422
Tawana2000 <murtezatawana8@gmail.com>
2025-09-27 13:16:29 -0700

    Update Bubble_sort_example4.py

--- SORTING METHODS/BUBBLE SORT/Bubble_sort_example4.py
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example4.py
@@ -1,2 +1,19 @@
 # Imagine you are analyzing race times (in seconds) for 8 runners: [54.3, 49.1, 61.0, 58.7, 47.5, 52.8, 60.2, 55.9], your task to:
 #1. Use bubble sort to sort the times in ascending order (fastest runner first), 2.Track how many comparisons are made in total (not swaps this time, but every time two elements are compared), 3.Return both the sorted list and the comparison count
+
+class Bubble_Sort:
+
+    def bubble_sort(self, lst):
+        self.lst = lst
+        size = len(lst)
+
+        for i in range(size):
+
+            for j in range(size - 1 - i):
+                if lst[j] > lst[j + 1]:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+        return lst, count
+    
+BBS = Bubble_Sort()
+print(BBS.bubble_sort([54.3, 49.1, 61.0, 58.7, 47.5, 52.8, 60.2, 55.9]))
