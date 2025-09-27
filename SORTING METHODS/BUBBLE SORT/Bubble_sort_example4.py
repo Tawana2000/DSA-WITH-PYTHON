57c92165cb6a478b595accd585557488d97709f1
Tawana2000 <murtezatawana8@gmail.com>
2025-09-27 13:15:21 -0700

    Create Bubble_sort_example4.py

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example4.py
@@ -0,0 +1,2 @@
+# Imagine you are analyzing race times (in seconds) for 8 runners: [54.3, 49.1, 61.0, 58.7, 47.5, 52.8, 60.2, 55.9], your task to:
+#1. Use bubble sort to sort the times in ascending order (fastest runner first), 2.Track how many comparisons are made in total (not swaps this time, but every time two elements are compared), 3.Return both the sorted list and the comparison count
