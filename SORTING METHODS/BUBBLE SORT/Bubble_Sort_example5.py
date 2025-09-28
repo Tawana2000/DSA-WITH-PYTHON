45875295cc167695955d415bc5ff9c314f45b510
Tawana2000 <murtezatawana8@gmail.com>
2025-09-28 14:42:15 -0700

    Create Bubble_Sort_example5.py

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_Sort_example5.py
@@ -0,0 +1,2 @@
+# You are analyzing a football team's performance. The players scored the following number of goals this season: [12, 5, 20, 7, 15, 3, 9].
+# Your task: 1.Use bubble sort to sort the list in ascending order, 2.While sorting, also count how many swaps were made in total, 3.Return both the sorted list and and the swap count
