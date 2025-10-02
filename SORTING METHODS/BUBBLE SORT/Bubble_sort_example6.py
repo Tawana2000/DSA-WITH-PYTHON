bfdb4455ec2dccf4f0be99e7c14677fa328eb781
Tawana2000 <murtezatawana8@gmail.com>
2025-10-02 15:01:51 -0700

    Update Bubble_sort_example6.py

--- SORTING METHODS/BUBBLE SORT/Bubble_sort_example6.py
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example6.py
@@ -3,3 +3,19 @@
 #Your task: 1.Use bubble sort to sort the scores in ascending order, 2.While sorting, track both: a.the total number of comparisons, b.The total number of swaps
 #3. Return the sorted list, comparisons, and swaps
 
+class BubbleSort:
+
+    def math_scores(self, scores):
+
+        self.scores = scores
+        size = len(scores)
+
+        for i in range(size):
+            for j in range(size - 1 - i):
+                if scores[j] > scores[j + 1]:
+                    scores[j], scores[j + 1] = scores[j + 1], scores[j]
+
+        return scores
+    
+BBS = BubbleSort()
+print(BBS.math_scores([72, 88, 95, 43, 67, 81, 59, 100, 76]))
