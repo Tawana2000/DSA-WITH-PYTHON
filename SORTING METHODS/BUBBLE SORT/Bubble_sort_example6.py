97d2711fe710a97ce5c4640fa024e00eb9e026b1
Tawana2000 <murtezatawana8@gmail.com>
2025-10-02 15:00:18 -0700

    Create Bubble_sort_example6.py

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example6.py
@@ -0,0 +1,5 @@
+# You are a teacher grading a math quiz. The students got these scores:
+#[72, 88, 95, 43, 67, 81, 59, 100, 76]
+#Your task: 1.Use bubble sort to sort the scores in ascending order, 2.While sorting, track both: a.the total number of comparisons, b.The total number of swaps
+#3. Return the sorted list, comparisons, and swaps
+
