9a790f0e867bac2d8a5a3d3f470493a60739a998
Tawana2000 <murtezatawana8@gmail.com>
2025-10-03 15:46:50 -0700

    Create Bubble_sort_example7.py

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example7.py
@@ -0,0 +1,19 @@
+# You have a list of students and their scores
+"""
+students = [
+    ["Alice", 72],
+    ["Bob", 88],
+    ["Charlie", 95],
+    ["David", 43],
+    ["Eva", 67],
+    ["Frank", 81],
+    ["Grace", 59],
+    ["Helen", 100],
+    ["Ian", 76]
+]
+"""
+#The task is to:
+#1.Use bubble sort to sort this list by score (the second element in each sublist) in ascending order.
+#2. Keep track of both comparisons and swaps
+#3. Return the sorted students list, the total comparisons, and the total swaps
+
