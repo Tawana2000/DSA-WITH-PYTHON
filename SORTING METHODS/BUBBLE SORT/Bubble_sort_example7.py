6de6e71b09214105a29f1be1e37a1ec22315b216
Tawana2000 <murtezatawana8@gmail.com>
2025-10-03 15:47:38 -0700

    Update Bubble_sort_example7.py

--- SORTING METHODS/BUBBLE SORT/Bubble_sort_example7.py
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example7.py
@@ -17,3 +17,28 @@ students = [
 #2. Keep track of both comparisons and swaps
 #3. Return the sorted students list, the total comparisons, and the total swaps
 
+
+class BubbleSort:
+
+    def scores_list(self, students):
+        self.students = students
+        size = len(students)
+        comparisons = 0
+        swaps = 0
+
+        for i in range(size):
+            swapped = False
+            for j in range(size - 1 - i):
+                comparisons += 1
+                if students[j][1] > students[j + 1][1]:
+                    students[j], students[j + 1] = students[j + 1], students[j]
+                    swapped = True
+                    swaps += 1
+                
+            if not swapped:
+                break
+
+        return students, comparisons, swaps
+
+BBS = BubbleSort()
+print(BBS.scores_list(students))
