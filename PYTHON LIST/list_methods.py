602721647e459a0091985e18aac789297d3aab5b
Tawana2000 <murtezatawana8@gmail.com>
2025-10-07 09:05:26 -0700

    Update list_methods.py

--- PYTHON LIST/list_methods.py
+++ PYTHON LIST/list_methods.py
@@ -16,7 +16,7 @@ wild_animals = ['Tiger', 'Coyote']
 
 # perform list operations
 animals.append('Raccoon')
-animals.extend(wild_animals)
+animals.extend(wild_animals) 
 animals.pop(2)
 animals.pop()
 animals.insert(2, 'Moose')
