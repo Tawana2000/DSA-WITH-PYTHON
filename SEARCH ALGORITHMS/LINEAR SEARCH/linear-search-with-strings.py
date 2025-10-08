374bb188c3b7aa2d9592fd6c6d3152180ef028b9
Tawana2000 <murtezatawana8@gmail.com>
2025-10-08 14:34:28 -0700

    Update linear-search-with-strings.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-with-strings.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-with-strings.py
@@ -1,1 +1,7 @@
 #Linear Search with Strings
+
+def linear_search_string(arr, target):
+    for i in range(len(arr)):
+        if arr[i].lower() == target.lower():  # Case-insensitive comparison
+            return i
+    return -1
