e297c62fe797d754976ef972a128903cb132aab3
Tawana2000 <murtezatawana8@gmail.com>
2025-10-08 14:34:44 -0700

    Update linear-search-with-strings.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-with-strings.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-with-strings.py
@@ -5,3 +5,9 @@ def linear_search_string(arr, target):
         if arr[i].lower() == target.lower():  # Case-insensitive comparison
             return i
     return -1
+
+names = ["Alice", "Bob", "Charlie", "David"]
+target = "charlie"
+
+result = linear_search_string(names, target)
+print("Found at index:", result if result != -1 else "Not found")
