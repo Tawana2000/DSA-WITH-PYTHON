2c8ce14493572357c8feb4a488d547828000b56b
Tawana2000 <murtezatawana8@gmail.com>
2025-10-11 00:09:28 -0700

    Create Sort-socks.py

---
+++ SORTING METHODS/BUBBLE SORT/Sort-socks.py
@@ -0,0 +1,2 @@
+#You run a small laundry service. After washing and drying, you end up with a pile of single socks. Each sock is tagged with a size number (e.g., 6, 8, 10, etc.). You want to sort the socks from smallest to largest by size before pairing them.
+#You decide to use a very simple sorting method — Bubble Sort — to arrange the socks.
