c2927fa939fdc406aebf336675258bd049490d1f
Tawana2000 <murtezatawana8@gmail.com>
2025-10-14 12:41:05 -0700

    Create Bubble_sort_example9.py

---
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example9.py
@@ -0,0 +1,4 @@
+#Sort employees by salary using Bubble Sort
+# You are given a list of employees, where each employee is represented as a tuple: (name, salary)
+# Write a Bubble Sort program to sort the employees by salary in descending order (highest-paid first).
+#Also count how many swaps the algorithm makes while sorting
