7a404d591a29e049f20f019c291e287db0e488b8
Tawana2000 <murtezatawana8@gmail.com>
2025-10-14 12:41:33 -0700

    Update Bubble_sort_example9.py

--- SORTING METHODS/BUBBLE SORT/Bubble_sort_example9.py
+++ SORTING METHODS/BUBBLE SORT/Bubble_sort_example9.py
@@ -2,3 +2,23 @@
 # You are given a list of employees, where each employee is represented as a tuple: (name, salary)
 # Write a Bubble Sort program to sort the employees by salary in descending order (highest-paid first).
 #Also count how many swaps the algorithm makes while sorting
+
+
+def bubble_sort_employee(employees):
+
+    n = len(employees)
+
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
+            if employees[j][1] > employees[j + 1][1]:
+                employees[j], employees[j + 1] = employees[j + 1], employees[j]
+                swapped = True
+                swaps += 1
+
+        if not swapped:
+            break
+
+    return employees
