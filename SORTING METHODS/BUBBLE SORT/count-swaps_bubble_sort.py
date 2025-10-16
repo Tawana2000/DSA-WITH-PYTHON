ed771a084b7201db51cb1727f74deb1669e41b61
Tawana2000 <murtezatawana8@gmail.com>
2025-10-15 23:39:54 -0700

    Implement swap counting in bubble sort
    Added a swap counter to track the number of swaps during bubble sort.

--- SORTING METHODS/BUBBLE SORT/count-swaps_bubble_sort.py
+++ SORTING METHODS/BUBBLE SORT/count-swaps_bubble_sort.py
@@ -6,13 +6,14 @@ class Bubble_Sort:
         self.lst = lst
         size = len(lst)
         swapped = False
-
+        count = 0
+        
         for i in range(size):
             for j in range(size - 1 - i):
                 if lst[j] > lst[j + 1]:
                     lst[j], lst[j + 1] = lst[j + 1], lst[j]
                     swapped = True
-                    
+                    count += 1
 
             if not swapped:
                 break
