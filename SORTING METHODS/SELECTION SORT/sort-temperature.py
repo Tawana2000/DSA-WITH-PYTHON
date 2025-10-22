24711721ecbb2c8b661a7568c32f20711ef58eb6
Tawana2000 <murtezatawana8@gmail.com>
2025-10-22 13:33:05 -0700

    Add selection sort function for temperature list
    Implemented selection sort to sort temperatures from highest to lowest.

--- SORTING METHODS/SELECTION SORT/sort-temperature.py
+++ SORTING METHODS/SELECTION SORT/sort-temperature.py
@@ -2,3 +2,18 @@
 # Write a python function using selection sort to sort these temperatures from highest to lowest, and thenm print
 # 1. The sorted list    2. The highest Temperature    3. The lowest temperature
 
+def sort_temp(temps):
+
+    for i in range(len(temps)):
+        min_max_temp = i
+
+        for j in range(i + 1, len(temps)):
+            if temps[j] > temps[min_max_temp]:
+                min_max_temp = j
+
+        temps[i], temps[min_max_temp] = temps[min_max_temp], temps[i]
+
+    return temps
+
+temps = [23, 19, 25, 22, 18, 24, 20]
+print(sort_temp(temps))
