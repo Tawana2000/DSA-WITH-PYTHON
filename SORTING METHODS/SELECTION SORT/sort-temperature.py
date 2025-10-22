1b63677f4c2fcbe45f82bf855fac584ec83ae0ff
Tawana2000 <murtezatawana8@gmail.com>
2025-10-22 13:31:21 -0700

    Create sort-temperature.py

---
+++ SORTING METHODS/SELECTION SORT/sort-temperature.py
@@ -0,0 +1,4 @@
+# You are given a list of temperatures (in C) recorded over a week: [23, 19, 25, 22, 18, 24, 20]
+# Write a python function using selection sort to sort these temperatures from highest to lowest, and thenm print
+# 1. The sorted list    2. The highest Temperature    3. The lowest temperature
+
