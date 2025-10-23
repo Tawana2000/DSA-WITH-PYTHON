e953379bfb80debeb233c22e7fb3d862c3f0d609
Tawana2000 <murtezatawana8@gmail.com>
2025-10-23 13:08:03 -0700

    Update selection-sort-ex2.py with function requirements
    Added function requirements for sorting and printing results.

--- SORTING METHODS/SELECTION SORT/selection-sort-ex2.py
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex2.py
@@ -1,3 +1,4 @@
 # You are organizing a running competition. The finish times (in seconds) of 8 participants are stored in a list:
 # [52, 47, 59, 45, 50, 49, 55, 48]
-
+# Write a python function that: 1.Sorts the times in ascending order (fastest to slowest), 2.Prints the top3 fastest runners, 3.Prints the slowest runner's time 
+# Use selection sort method
