3275fbc303de32be3220ffe96bf9ac34ce93464f
Tawana2000 <murtezatawana8@gmail.com>
2025-10-24 13:14:30 -0700

    Create selection-sort-ex3.py

---
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex3.py
@@ -0,0 +1,13 @@
+# You are managing a small store, and you have a list of products with their prices (in USD):
+"""
+products = [
+    ["Keyboard", 49],
+    ["Monitor", 120],
+    ["Mouse", 25],
+    ["Laptop", 899],
+    ["Headphones", 59],
+    ["USB Cable", 9]
+]
+"""
+# Write a python program that: 1.Sorts the products in ascending order by price using Selection Sort logic, 2.Prints the cheapest and most expensive product names, 3.Displays the entire sorted product list (name, price)
+
