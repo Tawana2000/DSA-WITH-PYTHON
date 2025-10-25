d292ac4507b5138b84daaa751242e2196647e051
Tawana2000 <murtezatawana8@gmail.com>
2025-10-25 13:34:10 -0700

    Add student data list for selection sort example
    Added example data for selection sort demonstration.

---
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex4.py
@@ -0,0 +1,11 @@
+# You are working in a school's data team, you have a list of students, each with a name and score out of 100:
+"""
+data = [
+    ["Ali", 85],
+    ["Sara", 92],
+    ["John", 78],
+    ["Fatima", 92],
+    ["Bilal", 85],
+    ["Zara", 60]
+]
+"""
