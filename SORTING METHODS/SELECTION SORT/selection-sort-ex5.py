446e796440311c0d8234703aab33f82361f470b1
Tawana2000 <murtezatawana8@gmail.com>
2025-10-26 13:47:02 -0700

    Create selection-sort-ex5.py

---
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex5.py
@@ -0,0 +1,19 @@
+# You are working in a company's HR analytics team, you have a list of employees with their names, departments, and monthly salaries (in USD):
+"""
+employees = [
+    ["Alice", "Engineering", 7200],
+    ["Bob", "HR", 4800],
+    ["Charlie", "Engineering", 6800],
+    ["Diana", "Finance", 7500],
+    ["Evan", "HR", 5000],
+    ["Fiona", "Finance", 6200]
+]
+"""
+# Write a python program that:
+"""
+1. Sorts the employees by salary (descending), highest earners first.
+2. If two employees have the same salary, sort them alphabetically by name.
+3. After sorting, print: 
+-> The top 3 highest earners (name + department + salary).
+-> The average salary of all employees
+"""
