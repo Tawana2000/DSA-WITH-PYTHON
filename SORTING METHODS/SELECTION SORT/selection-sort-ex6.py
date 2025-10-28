3b0e0e3ce82575161262fb2c31ff9f651361f590
Tawana2000 <murtezatawana8@gmail.com>
2025-10-28 04:08:56 -0700

    Add selection sort for student GPA sorting
    Implemented selection sort for sorting students by GPA and name.

--- SORTING METHODS/SELECTION SORT/selection-sort-ex6.py
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex6.py
@@ -9,3 +9,31 @@ students = [
     ["Mia", "Engineering", 3.9]
 ]
 """
+
+class Selection_Sort:
+
+    def student_gpa(self, gpa):
+
+        for i in range(len(gpa)):
+            min_max_gpa = i
+
+            for j in range(i + 1, len(gpa)):
+                if gpa[j][2] > gpa[min_max_gpa][2]:
+                    min_max_gpa = j
+                elif gpa[j][2] == gpa[min_max_gpa][2] and gpa[j][0] < gpa[min_max_gpa][0]:
+                    min_max_gpa = j
+
+            gpa[i], gpa[min_max_gpa] = gpa[min_max_gpa], gpa[i]
+        
+        return gpa
+    
+gpa = [
+    ["Zara", "Physics", 3.8],
+    ["Liam", "Math", 3.9],
+    ["Ava", "Biology", 3.8],
+    ["Noah", "Computer Science", 3.5],
+    ["Mia", "Engineering", 3.9]
+]
+
+SS = Selection_Sort()
+print(SS.student_gpa(gpa))
