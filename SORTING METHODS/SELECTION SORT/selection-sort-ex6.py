a60aaa31919d2e37c01a298c88e5915720b70c99
Tawana2000 <murtezatawana8@gmail.com>
2025-10-28 04:08:02 -0700

    Add selection sort implementation for student list
    Implement Selection Sort to arrange students by GPA and name.

---
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex6.py
@@ -0,0 +1,11 @@
+# You are given a list of students with their name, major, and GPA
+"""Write a Python program using Selection Sort to arrange the students from highest to lowest GPA
+If two students have the same GPA, sort them alphabetically by name:
+students = [
+    ["Zara", "Physics", 3.8],
+    ["Liam", "Math", 3.9],
+    ["Ava", "Biology", 3.8],
+    ["Noah", "Computer Science", 3.5],
+    ["Mia", "Engineering", 3.9]
+]
+"""
