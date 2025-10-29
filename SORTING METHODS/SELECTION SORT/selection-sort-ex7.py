004a35ed8ead05e14cbb0a93ad88ee774fb290aa
Tawana2000 <murtezatawana8@gmail.com>
2025-10-29 12:39:51 -0700

    Create selection-sort-ex7.py

---
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex7.py
@@ -0,0 +1,3 @@
+# You work at a digital library, Each book record includes: [title, author, year_published, rating]
+# Write a program using selection sort that: 1.Sorts the books in descending order of rating
+# 2.If two books have the same rating, sort them alphabetically by title, 3.Prints the sorted list in a clean format
