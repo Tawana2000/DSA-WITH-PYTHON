bd677dad9158e3b15c708ad81705783838dba771
Tawana2000 <murtezatawana8@gmail.com>
2025-10-30 12:35:37 -0700

    Update selection-sort-ex8.py

--- SORTING METHODS/SELECTION SORT/selection-sort-ex8.py
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex8.py
@@ -16,3 +16,41 @@ Using selection sort, sort the patients by the following rules (in priority orde
 Finally, print the sorted records neatly like:
 Name: John | Age: 72 | Urgency: 5 | Department: Cardiology | Arrival: 09:15
 """
+
+class Selection_Sort:
+
+    def hospital_management(self, patients):
+
+        for i in range(len(patients)):
+            low_high_urgency = i
+
+            for j in range(i + 1, len(patients)):
+                if patients[j][2] > patients[low_high_urgency][2]:
+                    low_high_urgency = j
+                elif patients[j][2] == patients[low_high_urgency][2] and patients[j][1] < patients[low_high_urgency][1]:
+                    low_high_urgency = j
+                elif patients[j][2] == patients[low_high_urgency][2] and patients[j][1] == patients[low_high_urgency][1] and patients[j][4] > patients[low_high_urgency][4]:
+                    low_high_urgency = j
+                elif patients[j][2] == patients[low_high_urgency][2] and patients[j][1] == patients[low_high_urgency][1] and patients[j][4] == patients[low_high_urgency][4] and patients[j][3] > patients[low_high_urgency][3]:
+                    low_high_urgency = j
+
+            patients[i], patients[low_high_urgency] = patients[low_high_urgency], patients[i]
+        
+        
+        for name, age, urgency, department, arrival_time in patients:
+            formatted_time = f"{arrival_time//100:02}:{arrival_time%100:02}"   
+            print(f"Name: {name} | Age: {age} | Urgency: {urgency} | Department: {department} | Arrival: {formatted_time}")
+        return patients
+patients = [
+    ["John", 72, 5, "Cardiology", 915],
+    ["Emily", 45, 4, "Neurology", 945],
+    ["Mark", 72, 5, "Orthopedics", 905],
+    ["Sophia", 30, 5, "General", 915],
+    ["David", 55, 4, "Cardiology", 930],
+    ["Lily", 60, 4, "Cardiology", 925],
+    ["Noah", 72, 5, "Cardiology", 915],
+    ["Ava", 72, 5, "Cardiology", 900]
+]
+
+SS = Selection_Sort()
+SS.hospital_management(patients)
