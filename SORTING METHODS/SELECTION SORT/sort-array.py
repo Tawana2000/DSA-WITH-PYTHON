00e6033d5a703744228555d74e29751c2a70ee84
Tawana2000 <murtezatawana8@gmail.com>
2025-11-01 19:41:25 -0700

    Create sort-array.py

---
+++ SORTING METHODS/SELECTION SORT/sort-array.py
@@ -0,0 +1,6 @@
+#Your task is to sort the array in descending order using the selection sort algorithm and also count the number of swaps performed.
+"""
+1.Implement selection sort to sort in descending order.
+2.Keep a counter for number of swaps.
+3.Print the sorted array and total swaps.
+"""
