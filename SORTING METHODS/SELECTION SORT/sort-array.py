a23437c54c1b38d41ab652fb6251247d7d1c1056
Tawana2000 <murtezatawana8@gmail.com>
2025-11-01 19:44:54 -0700

    Update sort-array.py

--- SORTING METHODS/SELECTION SORT/sort-array.py
+++ SORTING METHODS/SELECTION SORT/sort-array.py
@@ -4,3 +4,18 @@
 2.Keep a counter for number of swaps.
 3.Print the sorted array and total swaps.
 """
+
+class Selection_Sort:
+
+  def sort_array(self, array):
+
+    for i in range(len(array)):
+      min_max = i
+      for j in range(i + 1, len(array)):
+        if array[j] > array[min_max]:
+          min_max = j
+      array[i], array[min_max] = array[min_max], array[i]
+  return array
+
+SS = Selection_Sort()
+print(SS.sort_array([5, 2, 9, 1, 5, 6]))
