63af73e7a8c1ae449f05438d9909ddcfe3cf7e89
Tawana2000 <murtezatawana8@gmail.com>
2025-11-03 23:37:59 -0800

    Update selection-sort-ex9.py

--- SORTING METHODS/SELECTION SORT/selection-sort-ex9.py
+++ SORTING METHODS/SELECTION SORT/selection-sort-ex9.py
@@ -1,1 +1,14 @@
+#Job Applicant Ranking System
+"""
+You are building a system for a tech company that needs to rank applicants for inteviews.
+Each applicant record is:
+[name, degree_level, experience_years, test_score, interview_score, application_time]
 
+Sorting Rules (From highest to lowest)
+1. Degree level (highest degree first):
+       -PhD > Master's > Bachelors > Associate > High School
+2. Experience (more years first)
+3. Test Score (higher first)
+4. Interview Score (higher first)
+5. Application Time (earlier first)
+"""
