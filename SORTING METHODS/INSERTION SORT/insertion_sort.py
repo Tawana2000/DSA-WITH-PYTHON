11f98edcc7c788a8ebb824772967bb40bac7c1ae
Tawana2000 <murtezatawana8@gmail.com>
2025-11-07 13:48:53 -0800

    Update insertion_sort.py

--- SORTING METHODS/INSERTION SORT/insertion_sort.py
+++ SORTING METHODS/INSERTION SORT/insertion_sort.py
@@ -1,1 +1,14 @@
+#Insertion sort works by repeatedly
+"""
+- Taking an element from the unsorted portion of the list
+- And inserting it into its correct position within the already sorted portion
+"""
+#Working of insertion sort
+"""
+1. Divide the list into sorted and unsorted sublists
+- Initially we assume the first element is sorted (because there is only one element), and the rest of the list is unsorted.
+2. Set the first unsorted element  as the key element.
+3. Compare the key with the elements of the sorted sublist (moving from right to left) and insert it into its correct position.
+4. Repeat step 3 for all the elements of the unsorted part.
+"""
 
