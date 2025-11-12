87cba943433f9c984c13f01ed354d0ee863788a3
Tawana2000 <murtezatawana8@gmail.com>
2025-11-11 23:28:10 -0800

    Create insertion-sort-ex1.py

---
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex1.py
@@ -0,0 +1,10 @@
+# Trace the algorithm
+"""
+You are given the following list:
+arr = [9, 3, 7, 1, 5]
+Using insertion sort, show how the list changes after each outer loop iteration (after each i)
+Expected output:
+Step 1 (key = 3): [3, 9, 7, 1, 5]
+Step 2 (key = 7): [3, 7, 9, 1, 5]
+"""
+
