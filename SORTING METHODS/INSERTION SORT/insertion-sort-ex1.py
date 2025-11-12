473c746f1958f6c3d84339ffe44253c9c7bd40eb
Tawana2000 <murtezatawana8@gmail.com>
2025-11-11 23:28:27 -0800

    Update insertion-sort-ex1.py

--- SORTING METHODS/INSERTION SORT/insertion-sort-ex1.py
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex1.py
@@ -8,3 +8,22 @@ Step 1 (key = 3): [3, 9, 7, 1, 5]
 Step 2 (key = 7): [3, 7, 9, 1, 5]
 """
 
+class Insertion_Sort:
+
+    def trace_algorithm(self, arr):
+
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+
+            arr[j + 1] = key
+            print(f"Step {i} (Key = {key}): {arr}")
+        
+        return arr
+    
+IS = Insertion_Sort()
+IS.trace_algorithm([9, 3, 7, 1, 5])
