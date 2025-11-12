b1489c6c35844732f2477b363f6972a47d02ada5
Tawana2000 <murtezatawana8@gmail.com>
2025-11-11 23:29:10 -0800

    Create insertion-sort-ex2.py

---
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex2.py
@@ -0,0 +1,10 @@
+# Sort and Trace
+"""
+Given this list: [8, 4, 9, 2, 6]
+Your task:
+Write a Python class to:
+1. Perform Insertion Sort.
+2. Print the array after each insertion step.
+3. At the end, print the total number of shifts (how many times elements were moved right)
+"""
+
