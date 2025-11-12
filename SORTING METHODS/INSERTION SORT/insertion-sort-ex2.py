7c4505548a53cab1f38c10e540ae8c1d098a3d4e
Tawana2000 <murtezatawana8@gmail.com>
2025-11-11 23:29:25 -0800

    Update insertion-sort-ex2.py

--- SORTING METHODS/INSERTION SORT/insertion-sort-ex2.py
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex2.py
@@ -8,3 +8,24 @@ Write a Python class to:
 3. At the end, print the total number of shifts (how many times elements were moved right)
 """
 
+class Insertion_Sort:
+
+    def sort_trace(self, arr):
+
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+
+            arr[j + 1] = key
+            print(f"Step {i} (Key = {key}): {arr}")
+
+        print(f"Total Shifts: {i + 1}")
+        return arr
+        
+    
+IS = Insertion_Sort()
+IS.sort_trace([8, 4, 9, 2, 6])
