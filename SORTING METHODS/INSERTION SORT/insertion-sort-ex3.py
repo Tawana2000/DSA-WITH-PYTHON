6cdfc1d416b5e691cf738b1db75cd7898aa40f2d
Phantom <murtezatawana8@gmail.com>
2025-11-13 12:36:19 -0800

    Update insertion-sort-ex3.py

--- SORTING METHODS/INSERTION SORT/insertion-sort-ex3.py
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex3.py
@@ -11,3 +11,38 @@ The task is:
 -> Total number of comparisons
 -> Total number of shifts
 """
+
+class Insertion_Sort:
+
+    def compare_shift(self, arr):
+
+        comparisons = 0
+        shifts = 0
+
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+
+            # We count the comparison for entering the while-loop as well.
+            while True:
+                comparisons += 1  # for checking j >= 0
+                if j >= 0 and arr[j] > key:
+                    comparisons += 1  # for checking arr[j] > key
+                    arr[j + 1] = arr[j]
+                    shifts += 1
+                    j -= 1
+                else:
+                    break
+
+            arr[j + 1] = key
+
+            print(f"Step {i} (Key = {key}): {arr}")
+
+        print(f"Total Comparisons: {comparisons}")
+        print(f"Total Shifts: {shifts}")
+
+        return arr
+
+
+IS = Insertion_Sort()
+IS.compare_shift([12, 5, 7, 3, 11, 2])
