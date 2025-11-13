ba68c0c43e7604abe92e1c737b8c4a91a0b41322
Phantom <murtezatawana8@gmail.com>
2025-11-13 12:35:50 -0800

    Create insertion-sort-ex3.py

---
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex3.py
@@ -0,0 +1,13 @@
+# Track Comparisons and Shift
+"""
+Given the array: arr = [12, 5, 7, 3, 11, 2]
+The task is:
+- Perform insertion sort
+- After each step print:
+-> The step number
+-> The Key
+-> The array state
+- At the end, print:
+-> Total number of comparisons
+-> Total number of shifts
+"""
