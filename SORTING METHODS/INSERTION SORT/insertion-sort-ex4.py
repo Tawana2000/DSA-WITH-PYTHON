7947cb59c8a6516e9f78fcb024e60830057f820d
Phantom <murtezatawana8@gmail.com>
2025-11-25 13:21:20 -0800

    Create insertion-sort-ex4.py

---
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex4.py
@@ -0,0 +1,15 @@
+"""
+You are given an array of integers.
+Your task is to perform insertion sort, but with a twist:
+
+You must:
+
+Sort the array in ascending order using Insertion Sort
+Count the total number of comparisons made
+Count the total number of shifts (moves)
+Print the internal steps, including:
+Which key is being inserted
+Every shift operation
+The array after each step
+"""
+
