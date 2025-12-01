1716652eb8ebac97d56a4fde866d4e4abdc6d28c
Phantom <murtezatawana8@gmail.com>
2025-11-30 23:36:53 -0800

    Add insertion sort implementation for descending order
    Implemented insertion sort to sort a list in descending order.

--- SORTING METHODS/INSERTION SORT/insertion-sort-ex5.py
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex5.py
@@ -1,1 +1,20 @@
 # Write a program to sort the list items in descending order using insertion sort
+
+def insertion_sort(lst):
+    
+    for i in range(1, len(lst)):
+
+        key = lst[i]
+        j = i - 1
+
+        while j >= 0 and key > lst[j]:
+            lst[j + 1] = lst[j]
+
+            j = j - 1
+
+        lst[j + 1] = key
+
+    return lst
+
+lst = [1, 15, 6, 8, 2, 5, 9]
+print(insertion_sort(lst))
