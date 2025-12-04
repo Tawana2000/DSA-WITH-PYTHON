b56f6719de2bd9f12d87fc1f023d98ec5ce5726d
Phantom <murtezatawana8@gmail.com>
2025-12-04 13:26:31 -0800

    Add insertion sort for employee sorting
    Implement insertion sort for employee sorting based on salary, experience, and name.

---
+++ SORTING METHODS/INSERTION SORT/insertion-sort-ex6.py
@@ -0,0 +1,6 @@
+# Write a program to sort the employess using insertion sort, with the following rules:
+"""
+1. Higher salary comes first (descending)
+2. If salaries are equal, more years of experience comes first
+3. If both salary and experience are equal, sort alphabetically by name
+"""
