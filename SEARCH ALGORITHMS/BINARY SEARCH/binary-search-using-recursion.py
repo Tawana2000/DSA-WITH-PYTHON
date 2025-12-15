1fac9849c1f990381e87d64e45d4cff3bec07fcf
Phantom <murtezatawana8@gmail.com>
2025-12-14 19:24:32 -0800

    Update binary-search-using-recursion.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-using-recursion.py
@@ -10,7 +10,6 @@ def binary_search(lst, target, low, high):
         
         elif lst[mid] > target:
             return binary_search(lst, target, low, mid - 1)
-        
         else:
             return binary_search(lst, target, mid + 1, high)
         
