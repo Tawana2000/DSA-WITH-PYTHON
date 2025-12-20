d3670371054f9a0804707134d61a7f913f13012e
Phantom <murtezatawana8@gmail.com>
2025-12-20 12:59:58 -0800

    Create merge-sort-ex2.py

---
+++ SORTING METHODS/Merge Sort/merge-sort-ex2.py
@@ -0,0 +1,22 @@
+# You are given a list of employess, where each employee is represented as:
+"""
+(name, salary)
+
+Input:
+employees = [
+    ("Alice", 5200),
+    ("Bob", 4800),
+    ("Charlie", 6000),
+    ("David", 4500),
+    ("Eve", 5200)
+]
+
+Expected Output:
+[
+    ("David", 4500),
+    ("Bob", 4800),
+    ("Alice", 5200),
+    ("Eve", 5200),
+    ("Charlie", 6000)
+]
+"""
