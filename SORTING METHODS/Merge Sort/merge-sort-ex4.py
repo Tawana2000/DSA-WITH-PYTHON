d64525e52139674d1feb5b67e0865481b3e84955
Phantom <murtezatawana8@gmail.com>
2025-12-23 03:00:12 -0800

    Create merge-sort-ex4.py

---
+++ SORTING METHODS/Merge Sort/merge-sort-ex4.py
@@ -0,0 +1,15 @@
+#You are given a list of integers, sort the list in descending order using Merge Sort, print each merge step
+
+"""
+Input:
+arr = [5, 1, 8, 3, 2, 7]
+
+Expected Output:
+Merging [5] and [1] → [5, 1]
+Merging [8] and [3] → [8, 3]
+Merging [2] and [7] → [7, 2]
+Merging [5, 1] and [8, 3] → [8, 5, 3, 1]
+Merging [7, 2] and [8, 5, 3, 1] → [8, 7, 5, 3, 2, 1]
+
+Final Sorted Array: [8, 7, 5, 3, 2, 1]
+"""
