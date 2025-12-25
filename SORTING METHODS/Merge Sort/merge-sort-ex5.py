bc161ef539aa87887b7310b486d0d17a7dd35a15
Phantom <murtezatawana8@gmail.com>
2025-12-24 19:29:46 -0800

    Add merge sort implementation for student sorting
    Implement merge sort to sort students by grade and name.

---
+++ SORTING METHODS/Merge Sort/merge-sort-ex5.py
@@ -0,0 +1,28 @@
+# You are given a list of students, where each student is represented as: (name, grade)
+"""
+Sort Rules:
+1. Primary Key: grade -> descending
+2. Secondary Key: name -> ascending(A-Z)
+3. Use Merge Sort
+4. Do NOT use sorted() or .sort()
+
+
+Input:
+students = [
+    ("Alice", 85),
+    ("Bob", 92),
+    ("Charlie", 85),
+    ("David", 92),
+    ("Eve", 78)
+]
+
+
+Output:
+[
+    ("Bob", 92),
+    ("David", 92),
+    ("Alice", 85),
+    ("Charlie", 85),
+    ("Eve", 78)
+]
+"""
