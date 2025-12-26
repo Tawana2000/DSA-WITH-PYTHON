c1507e7d084102fcf57cad47f820a26dc16304d7
Phantom <murtezatawana8@gmail.com>
2025-12-26 06:52:54 -0800

    Add merge sort implementation for sorting points
    Implement merge sort to sort points by distance and coordinates.

--- SORTING METHODS/Merge Sort/merge-sort-ex7.py
+++ SORTING METHODS/Merge Sort/merge-sort-ex7.py
@@ -10,3 +10,56 @@ Sort Rules:
 3. Use Merge Sort only
 4. Do NOT use sorted() or .sort()
 """
+
+
+def distance(p):
+    return p[0] ** 2 + p[1]**2
+
+
+def merge_sort(points):
+
+    if len(points) <= 1:
+        return points
+    
+    mid = len(points) // 2
+
+    left_sorted = merge_sort(points[:mid])
+    right_sorted = merge_sort(points[mid:])
+
+    return merge(left_sorted, right_sorted)
+
+def merge(left, right):
+
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+
+        dist_left = distance(left[i])
+        dist_right = distance(right[j])
+
+        if dist_left < dist_right:
+            result.append(left[i])
+            i += 1
+
+        elif dist_left == dist_right and left[i][0] < right[j][0]:
+            result.append(left[i])
+            i += 1
+
+        elif (
+            dist_left == dist_right
+            and left[i][0] == right[j][0]
+            and left[i][1] <= right[j][1]
+        ):
+            result.append(left[i])
+            i += 1
+
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
