c6dba43feaf8bb913fc32056539fc7346a2be4ec
Phantom <murtezatawana8@gmail.com>
2025-12-26 06:52:21 -0800

    Create merge-sort-ex7.py

---
+++ SORTING METHODS/Merge Sort/merge-sort-ex7.py
@@ -0,0 +1,12 @@
+# You are given a list points on a 2D plane, represented as:
+"""
+(x, y)
+
+Sort Rules:
+1. Sort points by distance form the origin (0,0)v -> ascending
+2. If two points have the same distance, sort by:
+- x ascending
+- then y ascending
+3. Use Merge Sort only
+4. Do NOT use sorted() or .sort()
+"""
