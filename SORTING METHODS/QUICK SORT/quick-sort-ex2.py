92e284121fd21904fd41ee09b4cbc384c33b15fd
Phantom <murtezatawana8@gmail.com>
2025-12-27 05:41:40 -0800

    Add Quick Sort implementation for product sorting
    Implement Quick Sort for sorting products by price, rating, and name.

---
+++ SORTING METHODS/QUICK SORT/quick-sort-ex2.py
@@ -0,0 +1,7 @@
+# You are given a list of products, where each product is represented as: (name, price, rating)
+"""
+Sort the products using Quick Sort with these priorities
+- Lower price first
+- If prices are equal -> higher rating first
+- If both price and rating are equal -> Alphabetical name order
+"""
