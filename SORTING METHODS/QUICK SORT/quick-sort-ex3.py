832e65d70174a11e14d8ddf7ec95b0915d3e8e47
Phantom <murtezatawana8@gmail.com>
2025-12-28 22:15:51 -0800

    Create quick-sort-ex3.py

---
+++ SORTING METHODS/QUICK SORT/quick-sort-ex3.py
@@ -0,0 +1,31 @@
+# You are given a list of 2D points (x, y)
+"""
+Sort the points using Quick Sort based on:
+- Distance from the origin (0, 0), smaller distance first
+- If distance are equal -> Smaller x first
+- if x si also equal -> smaller y first
+
+
+Input
+
+points = [
+    (3, 4),
+    (1, 1),
+    (0, 2),
+    (2, 2),
+    (1, 2),
+    (0, 0)
+]
+
+
+Expected Output
+
+[
+    (0, 0),
+    (1, 1),
+    (0, 2),
+    (1, 2),
+    (2, 2),
+    (3, 4)
+]
+"""
