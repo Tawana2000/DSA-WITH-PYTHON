a4bd2b4f09472ec9fa56d879f1d9c1196fb03141
Phantom <murtezatawana8@gmail.com>
2025-12-31 09:27:02 -0800

    Implement linear search function to find target index

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex1.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex1.py
@@ -7,3 +7,16 @@ target = 2
 Output:
 1
 """
+
+def target_index(lst, target):
+
+    for index, element in enumerate(lst):
+        if element == target:
+            return index
+        
+    return None
+
+lst = [4, 2, 7, 2, 9, 2]
+target = 2
+
+print(target_index(lst, target))
