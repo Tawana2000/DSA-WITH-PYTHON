60e1f9e91a4db3ca73a7bdc4ff5e6e9e36a758bf
Phantom <murtezatawana8@gmail.com>
2026-01-01 23:04:39 -0800

    Add function to find all indexes of a target

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex2.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex2.py
@@ -8,3 +8,14 @@ Output:
 [1, 3, 5]
 """
 
+def all_indexes(lst, target):
+
+    indexes = []
+
+    for index, element in enumerate(lst):
+        if element == target:
+            indexes.append(index)
+    
+    return indexes
+
+print(all_indexes([4, 2, 7, 2, 9, 2], 2))
