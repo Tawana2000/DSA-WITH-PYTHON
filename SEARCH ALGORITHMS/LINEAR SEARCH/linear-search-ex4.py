c0907a38cdf064bc7abc055e708b9a7171ab4e46
Phantom <murtezatawana8@gmail.com>
2026-01-02 23:26:08 -0800

    Add function to find first element greater than target

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex4.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex4.py
@@ -7,3 +7,13 @@ target = 5
 Output:
 1
 """
+
+def first_greater(lst, target):
+
+    for index, element in enumerate(lst):
+        if element > target:
+            return index
+    
+    return None
+
+print(first_greater([3, 7, 1, 9, 4], 5))
