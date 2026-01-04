c842d0f7e0105de98537de1cc00f923ac4c8c15b
Phantom <murtezatawana8@gmail.com>
2026-01-03 22:22:07 -0800

    Update linear-search-ex6.py

--- SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex6.py
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex6.py
@@ -11,3 +11,25 @@ employees = [
 Output
 ("Charlie", 6000)
 """
+
+def highest_earner(employees):
+
+    if not employees:
+        return None
+    
+    highest = employees[0]
+
+    for employee in employees:
+        if employee[1] > highest[1]:
+            highest = employee
+
+    return highest
+
+employees = [
+    ("Alice", 5200),
+    ("Bob", 4800),
+    ("Charlie", 6000),
+    ("David", 4500)
+]
+
+print(highest_earner(employees))
