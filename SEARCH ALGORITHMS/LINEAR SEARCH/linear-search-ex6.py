f57644c1148b128352d3bc7f8b91a07ab84f0109
Phantom <murtezatawana8@gmail.com>
2026-01-03 22:21:31 -0800

    Create linear-search-ex6.py

---
+++ SEARCH ALGORITHMS/LINEAR SEARCH/linear-search-ex6.py
@@ -0,0 +1,13 @@
+# Find the first employee earning more than a given salary
+"""
+Input:
+employees = [
+    ("Alice", 5200),
+    ("Bob", 4800),
+    ("Charlie", 6000),
+    ("David", 4500)
+]
+
+Output
+("Charlie", 6000)
+"""
