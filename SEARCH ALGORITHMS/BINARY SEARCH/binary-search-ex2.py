5a9139a5926414df2f5d56f262d4e351b6d636a3
Phantom <murtezatawana8@gmail.com>
2026-01-07 11:44:30 -0800

    Create binary-search-ex2.py

---
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-ex2.py
@@ -0,0 +1,13 @@
+#Find the first Employee Earning at Least X
+"""
+You are given a sorted list of employee salaries (ascending order)
+Write a function that returns the index of the first employee whose salary is greater than or equal to a target salary
+If no such employee exists, return -1
+
+Example Input:
+salaries= [32000, 40000, 48000, 55000, 60000, 72000]
+target = 50000
+
+Expected Output:
+3
+"""
