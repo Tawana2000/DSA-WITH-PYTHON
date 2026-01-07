0849a31b2f79525be85c78c71780092bff04511b
Phantom <murtezatawana8@gmail.com>
2026-01-07 11:44:04 -0800

    Update binary-search-ex1.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search-ex1.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-ex1.py
@@ -14,3 +14,28 @@ Rules:
 - NO linear Scan 
 - NO built-in .index()
 """
+
+
+def delivery_times(slots, target):
+
+    left = 0 
+    right = len(slots) - 1
+    answer = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target < slots[mid]:
+            answer = mid
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return answer
+
+delivery_slots = [8, 9, 10, 12, 14, 16, 18]
+
+print(delivery_times(delivery_slots, 10))
+print(delivery_times(delivery_slots, 15))
+print(delivery_times(delivery_slots, 18))
