e25cc75d881ac6c5b3a1f7d217d42b0cfbf56ff5
Phantom <murtezatawana8@gmail.com>
2026-01-07 11:44:44 -0800

    Update binary-search-ex2.py

--- SEARCH ALGORITHMS/BINARY SEARCH/binary-search-ex2.py
+++ SEARCH ALGORITHMS/BINARY SEARCH/binary-search-ex2.py
@@ -11,3 +11,25 @@ target = 50000
 Expected Output:
 3
 """
+
+def first_salary_at_least(salaries, target):
+
+    left = 0
+    right = len(salaries) - 1
+    answer = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target <= salaries[mid]:
+            answer = mid
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return answer
+
+salaries = [32000, 40000, 48000, 55000, 60000, 72000]
+
+print(first_salary_at_least(salaries, 50000))
