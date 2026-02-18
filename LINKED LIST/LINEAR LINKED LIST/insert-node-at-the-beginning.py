d4ec4cd624b6509a46d4a2d406921b4b218cb74a
Phantom <murtezatawana8@gmail.com>
2026-02-18 13:45:45 -0800

    Update insert-node-at-the-beginning.py

--- LINKED LIST/LINEAR LINKED LIST/insert-node-at-the-beginning.py
+++ LINKED LIST/LINEAR LINKED LIST/insert-node-at-the-beginning.py
@@ -1,1 +1,38 @@
+# Write a program to insert a node at the beginning of an existing linked list
 
+class Node:
+    def __init__(self, data):
+         self.data = data
+         self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = Node
+
+    def create_linked_list(self):
+
+        node1 = Node(14)
+        node2 = Node(43)
+        node3 = Node(8)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+
+    def insert_node_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(f"{current.data}", end="->")
+            current = current.next
+        print(None)
+
+ll = Linkedlist()
+ll.create_linked_list()
+data = int(input("Enter the new node data: "))
+ll.insert_node_at_beginning(data)
+ll.display()
