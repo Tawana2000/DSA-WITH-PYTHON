85769a7ceacd6a6504c81e3eb663733bf9a32e11
Phantom <murtezatawana8@gmail.com>
2026-03-01 03:04:46 -0800

    Add Circular Linked List implementation
    Implemented a circular linked list with a method to create it.

--- LINKED LIST/CIRCULAR LINKED LIST/circular-linked-list.py
+++ LINKED LIST/CIRCULAR LINKED LIST/circular-linked-list.py
@@ -1,1 +1,29 @@
+# Create a Circular Linked List
+# To convert a regular linked list to a cirular linked list, we will make the last node point to the head node
 
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    #create circular linked list
+    def create_linked_list(self):
+        node1 = Node(80)
+        self.head = node1
+
+        node2 = Node(75)
+        node1.next = node2
+
+        node3 = Node(30)
+        node2.next = node3
+
+        #make the last node point back to the head node, making it circular
+        node3.next = self.head
+
+linked_list = CircularLinkedList()
+linked_list.create_linked_list()
