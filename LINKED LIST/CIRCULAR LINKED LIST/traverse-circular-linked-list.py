d7cf948572b7bf50f2acb86ae4f6709f4c015280
Phantom <murtezatawana8@gmail.com>
2026-03-03 13:19:45 -0800

    Add circular linked list traversal implementation
    Implemented a circular linked list with traversal functionality, including an is_empty method.

--- LINKED LIST/CIRCULAR LINKED LIST/traverse-circular-linked-list.py
+++ LINKED LIST/CIRCULAR LINKED LIST/traverse-circular-linked-list.py
@@ -1,1 +1,32 @@
+#Write a complete code for traversing a circular linked list
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    #Add an is_empty() method
+    def is_empty(self):
+        return self.head is None
+    
+    def traverse_list(self):
+
+        if self.is_empty():
+            print("Empty Linked LIst")
+            return
+        
+
+        current = self.head
+        while current.next is not self.head:
+            print(f"{current.data} ->", end=" ")
+            current = current.next
+        
+        #print the last node's as well
+        print(f"{current.data} -> {self.head.data}")
+
+linked_list = CircularLinkedList()
+linked_list.traverse_list()
