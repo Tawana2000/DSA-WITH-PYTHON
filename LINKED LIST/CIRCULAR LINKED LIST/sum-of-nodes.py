096bf59d27c56b7992a41ae40d66c5db10684a10
Phantom <murtezatawana8@gmail.com>
2026-03-05 03:17:05 -0800

    Add circular linked list implementation with sum method
    Implemented a circular linked list with methods to create the list, traverse it, and compute the sum of its nodes.

--- LINKED LIST/CIRCULAR LINKED LIST/sum-of-nodes.py
+++ LINKED LIST/CIRCULAR LINKED LIST/sum-of-nodes.py
@@ -1,1 +1,69 @@
+# Write a program to find the sum of all the nodes in a circular linked list:
+# You are provided with a function named compute_sum()
+# You need to add the data of all nodes and return the sum of the nodes
 
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    
+class CircularLinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    # Method to create a linked list
+    def create_linked_list(self):
+        
+        # Take input for node data
+        data1 = int(input())
+        data2 = int(input())
+        data3 = int(input())
+        data4 = int(input())
+
+        # Create 4 nodes with input values
+        node1 = Node(data1)
+        node2 = Node(data2)
+        node3 = Node(data3)
+        node4 = Node(data4)
+
+        # Set head field to the first node
+        self.head = node1
+
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+        node4.next = self.head
+
+    
+    def traverse_linked_list(self):
+        current = self.head
+        while current.next is not self.head:
+            print(f"{current.data} -> ", end="")
+            current = current.next
+        print(f"{current.data} -> {self.head.data}")
+
+
+    def compute_sum(self):
+        if self.head is None:
+            return 0
+        
+        current = self.head
+        total = 0
+
+        # Add all nodes except the last one
+        while current.next is not self.head:
+            total += current.data
+            current = current.next
+
+        # Add the last node
+        total += current.data
+
+        return total
+
+
+linked_list = CircularLinkedList()
+linked_list.create_linked_list()
+print(linked_list.compute_sum())
